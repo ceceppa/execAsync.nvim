2a1c7a5fa386060524b88e17c941dbcfb18a14b8
Alessandro Senese <senesealessandro@gmail.com>
2024-07-23 11:38:43 +0100

    move config to an object

--- lua/execAsync/execAsync.lua
+++ lua/execAsync/execAsync.lua
@@ -5,53 +5,76 @@ local show_notification = require('execAsync.notification')
 local M = {}
 local _latest_output = {}
 
-local function execute_command(cmd, on_completed_callback, is_silent)
-    local parts = vim.split(cmd, " ")
+local function execute_command(opts)
+    local command = opts.command
+    local is_silent = opts.is_silent
+    local on_complete = opts.on_complete
+    local on_stdout = opts.on_stdout
+
+    local parts = vim.split(command, " ")
     local command = parts[1]
     local args = vim.list_slice(parts, 2, #parts)
-    local description = 'Running: ' .. cmd
+    local description = 'Running: ' .. command
 
     _latest_output = {}
 
-    local on_complete = show_notification(description, is_silent)
+    local notification = show_notification(description, is_silent)
 
     Job:new({
         command = command,
         args = args,
         on_stdout = function(_, data)
             table.insert(_latest_output, data)
+
+            if on_stdout then
+                on_stdout(_latest_output)
+            end
         end,
         on_stderr = function(_, data)
             table.insert(_latest_output, data)
+
+            if on_stdout then
+                on_stdout(_latest_output)
+            end
         end,
         on_exit = function(_, return_val)
             _latest_output = _latest_output
 
-            if on_completed_callback then
+            if on_complete then
                 vim.schedule(function()
-                    on_completed_callback(_latest_output, return_val)
+                    on_complete(_latest_output, return_val)
                 end)
+
+            end
+
+            if on_stdout or notification then
+                notification("success")
+
+                return
             end
 
             if return_val == 0 then
-                on_complete("success")
+                notification("success")
             else
-                on_complete("error")
+                notification("error")
             end
         end
     }):start()
 end
 
-M.exec_async = function(command, on_completed_callback, is_silent)
-    if is_silent == nil then
-        is_silent = false
-    end
+M.exec_async = function(opts)
+    opts = vim.tbl_deep_extend("force", opts, {
+        command = nil,
+        is_silent = false,
+        on_complete = nil,
+        on_stdout = nil
+    })
+
+    local command = opts.command
 
     local ok, _ = pcall(
         execute_command,
-        command,
-        on_completed_callback,
-        is_silent
+        opts
     )
 
     if not ok then
